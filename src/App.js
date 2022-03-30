453bf9ec226efffb3ac2520f2b792f4dff284a1b
Rodrigo J <jimenezhuancarodrigo@gmail.com>
2022-03-30 01:18:56 -0400

    component Testimonio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Testimonio } from './components/testimonio.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className='contenedor-principal'>
+                <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp: </h1>
+                <Testimonio />
+            </div>
+        </div>
+    );
 }
 
 export default App;
